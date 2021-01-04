9e06581cee2697aeb4ee6713a578416af5d3127a
Lfh2msn <liufanghong@msn.com>
2021-01-04 19:31:05 +0800

    confusion

---
+++ application/models/Admin/AdminConfig.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class AdminConfig extends CI_Model {
+
+    //管理员
+    public function Manage($ak){
+        $admin_1 = "";//Admin User 1
+        $admin_2 = "";//Admin User 2
+        $admin_3 = "";//Admin User 3
+        $admin_4 = "";
+        $admin_5 = "";
+
+        if($ak==$admin_1||$ak==$admin_2||$ak==$admin_3){
+            return "ok";
+        }else{
+            return "err";
+        }
+    }
+}
